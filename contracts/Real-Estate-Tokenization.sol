02c22be99697b02b4db0a248dc2b9a0398f3a32c
Rohanindur06 <165511000+Rohanindur06@users.noreply.github.com>
2025-06-18 23:44:11 +0530

    Update Real-Estate-Tokenization.sol

--- contracts/Real-Estate-Tokenization.sol
+++ contracts/Real-Estate-Tokenization.sol
@@ -12,7 +12,7 @@ contract RealEstateTokenization is ERC721URIStorage, AccessControl {
     bytes32 public constant PROPERTY_MANAGER_ROLE = keccak256("PROPERTY_MANAGER_ROLE
         string location;
         uint256 area; // in square meters
-        uint256 valuation; // in wei (or USD equivalent)))
+        uint256 valuation; // in wei (or USD equivalen
     function mintProperty(
         address to,
         string memory tokenURI,
