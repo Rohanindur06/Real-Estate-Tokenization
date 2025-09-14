7f1bb9904cc2a8ef14308ae38efe0215a2c95329
Rohanindur06 <165511000+Rohanindur06@users.noreply.github.com>
2025-09-14 22:41:00 +0530

    Update Real-Estate-Tokenization.sol

--- contracts/Real-Estate-Tokenization.sol
+++ contracts/Real-Estate-Tokenization.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: mit
+// SPDX-License-Identifier: 
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -33,6 +33,7 @@ contract RealEstateTokenization is ERC721URIStorage, AccessControl {
         revokeRole(PROPERTY_MANAGER_ROLE, account);
     }
 }
+
 
 
 
